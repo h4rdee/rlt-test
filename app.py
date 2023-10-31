01661aa5fbe2f2b04c2279053a907ac9be8e5fbe
hardee <shegurov.danil.work@gmail.com>
2023-11-01 02:09:27 +0400

    chore(db): added db dump for further dockerization

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+import pymongo
+
 def main() -> None:
-  raise NotImplementedError("Nothing is implemented yet:(")
+  connection = pymongo.MongoClient("localhost", 27017, connectTimeoutMS=5000)
+  collection = connection["db"]["sample_collection"]
+  print(collection.find_one())
+  # raise NotImplementedError("Nothing is implemented yet:(")
 
 if __name__ == '__main__':
   main()
